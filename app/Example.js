06418fa8f089ca58b3d34039d4689feee0d5bcf6
sach11 <sachin.chopra@myntra.com>
2016-10-13 18:14:48 +0530

    added missing folders

---
+++ app/Example.js
@@ -0,0 +1,10 @@
+import React from 'react'
+require('../styles/main.less');
+
+export default class Example extends React.Component {
+    render(){
+		return(
+		   <div className="pinkHead"> Hello World  </div>
+		)
+    }
+}
